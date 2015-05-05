c5a0630d63e66f855922e8e80b8efafdaddfa5df
Alex <zhukeepa@gmail.com>
2015-05-05 13:49:22 -0400

    change from remote:true to $.ajax calls

--- app/assets/javascripts/categorizables.js
+++ app/assets/javascripts/categorizables.js
@@ -1,19 +1,26 @@
-var ready;
-ready = function() {
-  $('.topic-list .show .glyphicon-edit').on("click", function(e) {
-    //hide self, and get the location of edit from DOM
-    $('.topic-list .show').hide();
-    $('.topic-list .edit').show();
+var ready = function() {
+  $('.topic-list .links_list .glyphicon-edit').on("click", function(e) {
+    $(this).closest('.topic-list').find('.edit').show();
+    $(this).closest('.topic-list').find('.links_list').hide();
   });
-}
 
-function updateTopics(e) { 
-  //access contents of form submission
-  alert("foo");
-
-  // send post request
-
-  // reload the form ?? 
+  $('.topic-list .edit .btn').on("click", function(e) {
+    var section = $(this).closest('.topic-list'); 
+    var data = { 'klass': section.data('class'), 
+                 'id': section.data('id'), 
+                 'name': section.data('name'), 
+                 'topics_string': section.find('#categorizable___topics_string').val() };
+    
+    $.ajax({
+      url: '/categorizables/update_topic_list', 
+      data: JSON.stringify(data),
+      type: 'PATCH', 
+      contentType: 'application/json', 
+      success: function(response) { 
+        section.replaceWith(response); 
+      }
+    });
+  });
 }
 
 $(document).ready(ready);
