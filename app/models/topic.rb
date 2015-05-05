943b63c5133b6ade315b08d62cb6f7d3eae580b1
Alex <zhukeepa@gmail.com>
2015-05-05 14:56:28 -0400

    eager load everything, so there's no lazy loading problem with topics.

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -25,17 +25,16 @@ class Topic < ActiveRecord::Base
     (self.children.empty? ? [self] : self.children.map(&:descendants).flatten.append(self)).uniq
   end
 
-  # ::TODO:: this is SO HACKY there must be a better way...?? also, doesn't fully work...
-  def method_missing(m, *args, &block)
-    # e.g. if doing topic.problems, touch the Problem class, so that it loads and
-    # adds the problems method to Problem from acts_as_topicable. 
-    klass = m.to_s.titleize.gsub(' ', '').singularize.constantize
-    if Topic.instance_methods.include?(m)
-      self.send m
-    else
-      super 
-    end
-  rescue
-    super
-  end
+  # def method_missing(m, *args, &block)
+  #   # e.g. if doing topic.problems, touch the Problem class, so that it loads and
+  #   # adds the problems method to Problem from acts_as_topicable. 
+  #   klass = m.to_s.titleize.gsub(' ', '').singularize.constantize
+  #   if Topic.instance_methods.include?(m)
+  #     self.send m
+  #   else
+  #     super 
+  #   end
+  # rescue
+  #   super
+  # end
 end
